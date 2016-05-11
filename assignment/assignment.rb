2503ec9307ddd85833a648735ab3f7dd789c0c89
Guillermo Beltri <gobeltri@gmail.com>
2016-05-11 18:20:51 +0200

    rq05. Query DB with exact match

--- assignment/assignment.rb
+++ assignment/assignment.rb
@@ -50,7 +50,8 @@ class Assignment
       # accept a username input parameter
       # use the User Model class to find all Users with the supplied username. 
       # NOTE:  Username is not unique in the Users table, thus you can have many users with the same username.
-      # return a collection of User instances that match the provided username  
+      # return a collection of User instances that match the provided username
+      User.where(:username => username)  
   end 
 
   def find_todolist_byname(name)
@@ -58,6 +59,7 @@ class Assignment
       # use the TodoList Model class to find all TodoLists with the supplied list_name. 
       # NOTE: List name is not required to be unique, thus you can have many lists with the same list name.
       # return a collection of TodoList instances that match the provided name
+      TodoList.where(:list_name => name) 
   end     
 
   #
